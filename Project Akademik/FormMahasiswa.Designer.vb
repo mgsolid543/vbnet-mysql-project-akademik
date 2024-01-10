4b690a2f4a80d576989eecd07d2cbfa7ed1ba1f7
Irpan Budiana <irpan.budiana@gmail.com>
2024-01-10 20:04:00 +0700

    form mahasiswa

---
+++ Project Akademik/FormMahasiswa.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMahasiswa
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxNIM = New System.Windows.Forms.TextBox()
+        Me.TextBoxNama = New System.Windows.Forms.TextBox()
+        Me.ComboBoxJenisKelamin = New System.Windows.Forms.ComboBox()
+        Me.TextBoxAlamat = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ButtonSimpan = New System.Windows.Forms.Button()
+        Me.ButtonTambah = New System.Windows.Forms.Button()
+        Me.ButtonUbah = New System.Windows.Forms.Button()
+        Me.ButtonHapus = New System.Windows.Forms.Button()
+        Me.ButtonBatal = New System.Windows.Forms.Button()
+        Me.ButtonKeluar = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NIM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(50, 155)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(146, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jenis Kelamin"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(50, 224)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Alamat"
+        '
+        'TextBoxNIM
+        '
+        Me.TextBoxNIM.Location = New System.Drawing.Point(229, 37)
+        Me.TextBoxNIM.Name = "TextBoxNIM"
+        Me.TextBoxNIM.Size = New System.Drawing.Size(133, 31)
+        Me.TextBoxNIM.TabIndex = 4
+        '
+        'TextBoxNama
+        '
+        Me.TextBoxNama.Location = New System.Drawing.Point(229, 94)
+        Me.TextBoxNama.Name = "TextBoxNama"
+        Me.TextBoxNama.Size = New System.Drawing.Size(281, 31)
+        Me.TextBoxNama.TabIndex = 5
+        '
+        'ComboBoxJenisKelamin
+        '
+        Me.ComboBoxJenisKelamin.FormattingEnabled = True
+        Me.ComboBoxJenisKelamin.Location = New System.Drawing.Point(229, 155)
+        Me.ComboBoxJenisKelamin.Name = "ComboBoxJenisKelamin"
+        Me.ComboBoxJenisKelamin.Size = New System.Drawing.Size(188, 33)
+        Me.ComboBoxJenisKelamin.TabIndex = 6
+        '
+        'TextBoxAlamat
+        '
+        Me.TextBoxAlamat.Location = New System.Drawing.Point(229, 221)
+        Me.TextBoxAlamat.Name = "TextBoxAlamat"
+        Me.TextBoxAlamat.Size = New System.Drawing.Size(675, 31)
+        Me.TextBoxAlamat.TabIndex = 7
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(55, 275)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.Size = New System.Drawing.Size(849, 287)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'ButtonSimpan
+        '
+        Me.ButtonSimpan.Location = New System.Drawing.Point(55, 581)
+        Me.ButtonSimpan.Name = "ButtonSimpan"
+        Me.ButtonSimpan.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonSimpan.TabIndex = 9
+        Me.ButtonSimpan.Text = "Simpan"
+        Me.ButtonSimpan.UseVisualStyleBackColor = True
+        '
+        'ButtonTambah
+        '
+        Me.ButtonTambah.Location = New System.Drawing.Point(190, 581)
+        Me.ButtonTambah.Name = "ButtonTambah"
+        Me.ButtonTambah.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonTambah.TabIndex = 10
+        Me.ButtonTambah.Text = "Tambah"
+        Me.ButtonTambah.UseVisualStyleBackColor = True
+        '
+        'ButtonUbah
+        '
+        Me.ButtonUbah.Location = New System.Drawing.Point(325, 581)
+        Me.ButtonUbah.Name = "ButtonUbah"
+        Me.ButtonUbah.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonUbah.TabIndex = 11
+        Me.ButtonUbah.Text = "Ubah"
+        Me.ButtonUbah.UseVisualStyleBackColor = True
+        '
+        'ButtonHapus
+        '
+        Me.ButtonHapus.Location = New System.Drawing.Point(484, 581)
+        Me.ButtonHapus.Name = "ButtonHapus"
+        Me.ButtonHapus.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonHapus.TabIndex = 12
+        Me.ButtonHapus.Text = "Hapus"
+        Me.ButtonHapus.UseVisualStyleBackColor = True
+        '
+        'ButtonBatal
+        '
+        Me.ButtonBatal.Location = New System.Drawing.Point(619, 581)
+        Me.ButtonBatal.Name = "ButtonBatal"
+        Me.ButtonBatal.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonBatal.TabIndex = 13
+        Me.ButtonBatal.Text = "Batal"
+        Me.ButtonBatal.UseVisualStyleBackColor = True
+        '
+        'ButtonKeluar
+        '
+        Me.ButtonKeluar.Location = New System.Drawing.Point(775, 581)
+        Me.ButtonKeluar.Name = "ButtonKeluar"
+        Me.ButtonKeluar.Size = New System.Drawing.Size(129, 48)
+        Me.ButtonKeluar.TabIndex = 14
+        Me.ButtonKeluar.Text = "Keluar"
+        Me.ButtonKeluar.UseVisualStyleBackColor = True
+        '
+        'FormMahasiswa
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(964, 690)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.ButtonKeluar)
+        Me.Controls.Add(Me.ButtonBatal)
+        Me.Controls.Add(Me.ButtonHapus)
+        Me.Controls.Add(Me.ButtonUbah)
+        Me.Controls.Add(Me.ButtonTambah)
+        Me.Controls.Add(Me.ButtonSimpan)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBoxAlamat)
+        Me.Controls.Add(Me.ComboBoxJenisKelamin)
+        Me.Controls.Add(Me.TextBoxNama)
+        Me.Controls.Add(Me.TextBoxNIM)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "FormMahasiswa"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Master Data Mahasiswa"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxNIM As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxNama As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBoxJenisKelamin As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBoxAlamat As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents ButtonSimpan As System.Windows.Forms.Button
+    Friend WithEvents ButtonTambah As System.Windows.Forms.Button
+    Friend WithEvents ButtonUbah As System.Windows.Forms.Button
+    Friend WithEvents ButtonHapus As System.Windows.Forms.Button
+    Friend WithEvents ButtonBatal As System.Windows.Forms.Button
+    Friend WithEvents ButtonKeluar As System.Windows.Forms.Button
+End Class
---
+++ Project Akademik/FormMahasiswa.vb
@@ -0,0 +1,196 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormMahasiswa
+    Public strSQL As String
+    Public DB As MySqlConnection
+    Public CMD As MySqlCommand
+    Public ADP As MySqlDataAdapter
+    Public DR As MySqlDataReader
+    Public DS As New DataSet
+
+    Public simpan, hapus As String
+
+    Sub tambah()
+        DataGridView1.Enabled = False
+        TextBoxNIM.ReadOnly = False
+
+        TextBoxNIM.Clear()
+        TextBoxNama.Clear()
+        TextBoxAlamat.Clear()
+
+        TextBoxNIM.Enabled = True
+        TextBoxNama.Enabled = True
+        ComboBoxJenisKelamin.Enabled = True
+        TextBoxAlamat.Enabled = True
+
+        TextBoxNIM.Focus()
+
+        ButtonTambah.Enabled = False
+        ButtonHapus.Enabled = False
+        ButtonUbah.Enabled = True
+        ButtonSimpan.Enabled = True
+        ButtonBatal.Enabled = True
+    End Sub
+
+    Sub ubah()
+        TextBoxNIM.Enabled = True
+        TextBoxNama.Enabled = True
+        ComboBoxJenisKelamin.Enabled = True
+        TextBoxAlamat.Enabled = True
+        TextBoxNIM.Focus()
+
+        ButtonTambah.Enabled = True
+        ButtonHapus.Enabled = True
+        ButtonUbah.Enabled = True
+        ButtonSimpan.Enabled = False
+        ButtonBatal.Enabled = True
+    End Sub
+
+    Sub batal()
+        DataGridView1.Enabled = True
+        TextBoxNIM.Clear()
+        TextBoxNama.Clear()
+        TextBoxAlamat.Clear()
+
+        TextBoxNIM.Enabled = True
+        TextBoxNama.Enabled = True
+        ComboBoxJenisKelamin.Enabled = True
+        TextBoxAlamat.Enabled = True
+        TextBoxNIM.Focus()
+
+        ButtonTambah.Enabled = True
+        ButtonHapus.Enabled = True
+        ButtonUbah.Enabled = True
+        ButtonSimpan.Enabled = False
+        ButtonBatal.Enabled = True
+    End Sub
+
+    Sub bersih()
+        TextBoxNIM.Clear()
+        TextBoxNama.Clear()
+        TextBoxAlamat.Clear()
+        TextBoxNIM.Focus()
+    End Sub
+
+    Sub isiGrid()
+        ModuleKoneksi.bukaDB()
+        DA = New MySqlDataAdapter("select * from mahasiswa", Koneksi)
+
+        ds = New DataSet
+        DA.Fill(DS, "mahasiswa")
+
+        DataGridView1.DataSource = DS.Tables("mahasiswa")
+        DataGridView1.ReadOnly = True
+    End Sub
+
+    Private Sub ButtonKeluar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonKeluar.Click
+        FormUtama.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub FormMahasiswa_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call isiGrid()
+        Call ubah()
+    End Sub
+
+    Private Sub ButtonTambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonTambah.Click
+        Call tambah()
+    End Sub
+
+    Private Sub ButtonSimpan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSimpan.Click
+        Call bukaDB()
+        CMD = New MySqlCommand("select nim from mahasiswa where nim = '" & TextBoxNIM.Text & "'", Koneksi)
+        RD = CMD.ExecuteReader
+        RD.Read()
+
+        If ButtonTambah.Enabled = False Then
+            If RD.HasRows Then
+                MsgBox("Maaf, Data dengan NIM tersebut telah ada", MsgBoxStyle.Exclamation, "Peringatan")
+            Else
+                If (MessageBox.Show("Simpan data mahasiswa baru?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) = DialogResult.Yes Then
+                    Call bukaDB()
+                    simpan = "insert into mahasiswa (nim, nama, jeniskelamin, alamat) values ('" & TextBoxNIM.Text & "', '" & TextBoxNama.Text & "', '" & ComboBoxJenisKelamin.Text & "','" & TextBoxAlamat.Text & "')"
+
+                    CMD = New MySqlCommand(simpan, Koneksi)
+
+                    CMD.ExecuteNonQuery()
+
+                    Call isiGrid()
+                    Call batal()
+                    TextBoxNIM.Focus()
+
+                    MsgBox("Data mahasiswa sudah tersimpan.", MsgBoxStyle.Information, "Info")
+                    RD.Close()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonUbah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonUbah.Click
+        bukaDB()
+        Try
+            Using conn As New MySqlConnection(Koneksi.ConnectionString)
+                conn.Open()
+                Dim command As New MySqlCommand("update mahasiswa set nim=@nim, nama=@nama, jeniskelamin=@jeniskelamin, alamat=@alamat where nim=@nim", conn)
+
+                With command.Parameters
+                    .AddWithValue("@nim", TextBoxNIM.Text)
+                    .AddWithValue("@nama", TextBoxNama.Text)
+                    .AddWithValue("@jeniskelamin", ComboBoxJenisKelamin.Text)
+                    .AddWithValue("@alamat", TextBoxAlamat.Text)
+                End With
+
+                command.ExecuteNonQuery()
+                MessageBox.Show("Data mahaiswa sukses tersimpan", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Kesalahan dalam ubah data", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Call isiGrid()
+        Call bersih()
+    End Sub
+
+    Private Sub ButtonBatal_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonBatal.Click
+        Call batal()
+    End Sub
+
+    Private Sub ButtonHapus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonHapus.Click
+        If TextBoxNIM.Text = "" Then
+            MessageBox.Show("Pilih dahulu data mahasiswa yang akan Anda hapus pada tabel di atas!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            If TextBoxNIM.Text <> "" Then
+                If MessageBox.Show("Apakah Anda yakin akan mengahapus data mahasiswa ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+                    Call bukaDB()
+                    hapus = "delete from mahasiswa where nim = '" & TextBoxNIM.Text & "'"
+                    CMD = New MySqlCommand(hapus, Koneksi)
+
+                    CMD.ExecuteNonQuery()
+                    Call isiGrid()
+                    Call bersih()
+                    TextBoxNIM.Focus()
+                    MessageBox.Show("Data ruangan berhasil dihapus", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        TextBoxNIM.ReadOnly = True
+        bukaDB()
+        CMD = New MySqlCommand("select * from mahasiswa where nim = '" & TextBoxNIM.Text & "'", Koneksi)
+
+        Try
+            Dim gridbaris As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+
+            TextBoxNIM.Text = gridbaris.Cells(0).Value.ToString
+            TextBoxNama.Text = gridbaris.Cells(1).Value.ToString
+            ComboBoxJenisKelamin.Text = gridbaris.Cells(2).Value.ToString
+            TextBoxAlamat.Text = gridbaris.Cells(3).Value.ToString
+
+        Catch ex As Exception
+            MsgBox("Pilih data mahasiswa yang ada di dalam tabel", MsgBoxStyle.Information, "Info")
+        End Try
+    End Sub
+
+
+End Class
